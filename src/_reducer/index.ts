de888f2b6caf4066e00cdd6df8a202223b339209
SoonKi Kwon <jhk327@gmail.com>
2021-11-13 23:52:12 +0900

    useFilteredTodos hook

--- src/_reducer/index.ts
+++ src/_reducer/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { useSelector } from 'react-redux';
 import filter from './filter';
+import todos from './todos';
 
 const rootReducer = combineReducers({
   filter,
+  todos,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
---
+++ src/hooks/useFilteredTodos.ts
@@ -0,0 +1,17 @@
+import { useMemo } from 'react';
+import { useRootState } from '../_reducer';
+import { useFilter } from './useFilter';
+import { useTodoActions } from './useTodoActions';
+
+export function useFilteredTodos() {
+  const todos = useRootState((state) => state.todos);
+  const [filter] = useFilter();
+
+  const filteredTodos = useMemo(
+    () => (filter === 'ALL' ? todos : todos.filter((todo) => todo.done === (filter === 'DONE'))),
+    [todos, filter]
+  );
+  const actions = useTodoActions();
+
+  return [filteredTodos, actions];
+}
