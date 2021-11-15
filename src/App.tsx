4a35f9e39801b4a4bb0852b0f3a43cb6c51aa183
SoonKi Kwon <jhk327@gmail.com>
2021-11-15 23:56:50 +0900

    TodoForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 export default function App() {
   return (
     <>
+      <TodoForm />
       <TodoList />
     </>
   );
