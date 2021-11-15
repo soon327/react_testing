06b47c122d9471cefeebb1f6bdc209c71c369653
SoonKi Kwon <jhk327@gmail.com>
2021-11-15 23:58:30 +0900

    TodoFilters component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
+import TodoFilters from './components/TodoFilters';
 
 export default function App() {
   return (
     <>
       <TodoForm />
       <TodoList />
+      <TodoFilters />
     </>
   );
 }
