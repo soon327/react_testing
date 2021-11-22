23a2f91b20b4cf7ae8fd55e8a3a41bba248601c0
SoonKi Kwon <jhk327@gmail.com>
2021-11-22 22:41:00 +0900

    setting alias root/src

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   testEnvironment: 'node',
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   testMatch: ['<rootDir>/**/*.test.(js|jsx|ts|tsx)', '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'],
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
